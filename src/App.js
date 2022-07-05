dbbbffdc89cb084053881103e1ec04bc7143ae1a
Kush Goyal <82465969+kushgoyal12@users.noreply.github.com>
2022-07-05 16:24:27 +0530

    integrated context api :fire:

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromResponse } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
+
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
 
-  useEffect(() => {
+  useEffect(() => { 
     const hash = getTokenFromResponse();
     
     window.location.hash = "";
@@ -14,13 +21,20 @@ function App() {
     if(_token)
     {
       setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("person", user);
+      });
     }
+
     console.log("Token  ",_token)
     
   }, []);
   return (
     <div>
-      {token ? (<h1>I have the token</h1>)
+      {token ? 
+      <Player />
       : <Login />
       }
       
